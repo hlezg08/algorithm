ce174c8858e0b00f866ffba699b1fc2df27ec22e
hlezg08 <hyelin132@gmail.com>
2022-10-29 22:21:03 +0900

    LeetCode Max Area of Island solved
    count 변수 nonlocal로 해결

--- search/max-area-of-island.py
+++ search/max-area-of-island.py
@@ -2,19 +2,25 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         n=len(grid)
         m=len(grid[0])
-       
+        maxArea=0
         count=0
+        
         def dfs(x,y):
+            nonlocal count
             if(x<0 or x>=n or y<0 or y>=m): return
-            if(grid[x][y]==0): return
-            dfs(x-1,y)
-            dfs(x+1,y)
-            dfs(x,y-1)
-            dfs(x,y+1)
-            count+=1
+            if(grid[x][y]==1): 
+                grid[x][y]=0
+                count+=1
+                dfs(x-1,y)
+                dfs(x+1,y)
+                dfs(x,y-1)
+                dfs(x,y+1)
             return count
             
         for i in range(n):
             for j in range(m):
                 if(grid[i][j]==1):
-                    print(dfs(i,j))
+                    maxArea=max(maxArea,dfs(i,j))
+                    count=0
+                    
+        return maxArea
